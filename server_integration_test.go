4e33716f40819898770d88dca8ff3268c186fa39
Thitikorn Ongethphol <taton825@hotmail.com>
2022-12-27 16:27:34 +0700

    change solution to validate header:Authorization and add method authMiddleware to server.go

--- server_integration_test.go
+++ server_integration_test.go
@@ -64,7 +64,7 @@ func uri(paths ...string) string {
 
 func request(method, url string, body io.Reader) *Response {
 	req, _ := http.NewRequest(method, url, body)
-	req.Header.Add("Authorization", "Bearer "+os.Getenv("AUTH_TOKEN"))
+	req.Header.Add("Authorization", os.Getenv("AUTH_TOKEN"))
 	req.Header.Add("Content-Type", "application/json")
 	client := http.Client{}
 	res, err := client.Do(req)
