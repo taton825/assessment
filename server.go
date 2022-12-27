dea79c326a595a88c3d391d896ba2613226cc07f
Thitikorn Ongethphol <taton825@hotmail.com>
2022-12-28 02:11:04 +0700

    refactor by extract file to get.go

--- server.go
+++ server.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/lib/pq"
 	"github.com/taton825/assessment/config"
 	"github.com/taton825/assessment/database"
 	"github.com/taton825/assessment/expense"
@@ -29,24 +28,6 @@ func authMiddleware() echo.MiddlewareFunc {
 	}
 }
 
-func getExpenseHandler(c echo.Context) error {
-	rowid := c.Param("id")
-
-	stmt, err := database.DB.Prepare("SELECT id, title, amount, note, tags FROM expenses WHERE id = $1")
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, expense.Err{Message: "can't create prepare statement for get one expense: " + err.Error()})
-	}
-
-	row := stmt.QueryRow(rowid)
-	// log.Println("row:" + row.Err().Error())
-	var e expense.Expense
-	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, expense.Err{Message: "can't scan row into variable: " + err.Error()})
-	}
-	return c.JSON(http.StatusOK, e)
-}
-
 func main() {
 
 	fmt.Println("Please use server.go for main file")
@@ -63,7 +44,7 @@ func main() {
 	e.Use(authMiddleware())
 
 	e.POST("/expenses", expense.CreateExpenseHandler)
-	e.GET("/expenses/:id", getExpenseHandler)
+	e.GET("/expenses/:id", expense.GetExpenseHandler)
 
 	log.Println("Server started at :", os.Getenv("PORT"))
 	log.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
