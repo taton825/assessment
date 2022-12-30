70b7009a6b6915f9c80b2d114086d59d4c36eb3e
Thitikorn Ongethphol <taton825@hotmail.com>
2022-12-30 17:40:39 +0700

    extract file & function getExpensesHandler

--- server.go
+++ server.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/lib/pq"
 	"github.com/taton825/assessment/config"
 	"github.com/taton825/assessment/database"
 	"github.com/taton825/assessment/expense"
@@ -29,32 +28,6 @@ func authMiddleware() echo.MiddlewareFunc {
 	}
 }
 
-func getExpensesHandler(c echo.Context) error {
-	stmt, err := database.DB.Prepare("SELECT id, title, amount, note, tags FROM expenses")
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, expense.Err{Message: "can't create prepare statement for get one expense: " + err.Error()})
-	}
-
-	rows, err := stmt.Query()
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, expense.Err{Message: "can't query all expenses:" + err.Error()})
-	}
-
-	var expenses []expense.Expense
-
-	for rows.Next() {
-		var e expense.Expense
-		err := rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, expense.Err{Message: "can't scan row into variable:" + err.Error()})
-		}
-		expenses = append(expenses, e)
-	}
-	log.Println("query all expenses success")
-
-	return c.JSON(http.StatusOK, expenses)
-}
-
 func main() {
 
 	fmt.Println("Please use server.go for main file")
@@ -73,7 +46,7 @@ func main() {
 	e.POST("/expenses", expense.CreateExpenseHandler)
 	e.GET("/expenses/:id", expense.GetExpenseHandler)
 	e.PUT("/expenses/:id", expense.PutExpenseHandler)
-	e.GET("/expenses", getExpensesHandler)
+	e.GET("/expenses", expense.GetExpensesHandler)
 
 	log.Println("Server started at :", os.Getenv("PORT"))
 	log.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
