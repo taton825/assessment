39e1d58057413cd10feb7bb24b64d417aff46d86
Thitikorn Ongethphol <taton825@hotmail.com>
2022-12-30 15:57:01 +0700

    Refactoring put expense

--- server.go
+++ server.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/lib/pq"
 	"github.com/taton825/assessment/config"
 	"github.com/taton825/assessment/database"
 	"github.com/taton825/assessment/expense"
@@ -29,24 +28,6 @@ func authMiddleware() echo.MiddlewareFunc {
 	}
 }
 
-func putExpenseHandler(c echo.Context) error {
-	rowid := c.Param("id")
-
-	var e expense.Expense
-	err := c.Bind(&e)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, expense.Err{Message: err.Error()})
-	}
-
-	row := database.DB.QueryRow("UPDATE expenses SET title = $2, amount = $3, note = $4, tags = $5 WHERE id = $1 RETURNING id", rowid, e.Title, e.Amount, e.Note, pq.Array(e.Tags))
-
-	err = row.Scan(&e.ID)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, expense.Err{Message: err.Error()})
-	}
-	return c.JSON(http.StatusOK, e)
-}
-
 func main() {
 
 	fmt.Println("Please use server.go for main file")
@@ -64,7 +45,7 @@ func main() {
 
 	e.POST("/expenses", expense.CreateExpenseHandler)
 	e.GET("/expenses/:id", expense.GetExpenseHandler)
-	e.PUT("/expenses/:id", putExpenseHandler)
+	e.PUT("/expenses/:id", expense.PutExpenseHandler)
 
 	log.Println("Server started at :", os.Getenv("PORT"))
 	log.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
